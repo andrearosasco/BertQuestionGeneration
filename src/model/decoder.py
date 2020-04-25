3a65077938af79e441256be3c3c7adad2185c1b6
andrea-rosasco <andrearosasco.ar@gmail.com>
2020-04-25 13:39:13 +0200

    fixed decoder interface to match the one in master

--- src/model/decoder.py
+++ src/model/decoder.py
@@ -5,39 +5,36 @@ import torch.nn.functional as F
 import torch
 
 class Decoder(nn.Module):
-    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout, attention):
+    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, num_layers, dropout, attention, device):
         super().__init__()
+
+        self.device = device
 
         self.emb_dim = emb_dim
         self.enc_hid_dim = enc_hid_dim
         self.dec_hid_dim = dec_hid_dim
         self.output_dim = output_dim
-        self.dropout = dropout
+        self.num_layers = num_layers
         self.attention = attention
-
+        self.dropout = nn.Dropout(dropout)
         self.embedding = nn.Embedding(output_dim, emb_dim)
 
-        self.rnn = nn.GRU(enc_hid_dim + emb_dim, dec_hid_dim, batch_first=True)
+        self.rnn = nn.GRU(enc_hid_dim + emb_dim, dec_hid_dim, batch_first=True, num_layers=num_layers, dropout=dropout)
         #  The input will be the concat between attention result and input
 
         self.out = nn.Linear(enc_hid_dim + dec_hid_dim + emb_dim, output_dim)
 
-        self.dropout = nn.Dropout(dropout)
+    def forward(self, src, queries, hidden):
+        # src = [batch size]
+        # queries = [batch size, src sent len, enc hid dim]
+        # hidden = [batch size, dec_hid_dim]
 
-    def forward(self, input, queries, key=None):
-        # input = [batch size]
-        # queries = [batch size, dec hid dim]
-        # encoder_outputs = [src sent len, batch size, enc hid dim * 2]
-        if key is None:
-            key = torch.zeros(input.shape[0], self.dec_hid_dim)
-
-        input = input.unsqueeze(1)
+        src = src.unsqueeze(1)
         # input = [batch size, senq len]
 
-        embedded = self.dropout(self.embedding(input))
+        embedded = self.dropout(self.embedding(src))
         # embedded = [batch size, seq len, emb dim]
-
-        a = self.attention(key, queries)
+        a = self.attention(hidden, queries)
 
         # a = [batch size, src len]
         a = a.unsqueeze(1)
@@ -51,7 +48,7 @@ class Decoder(nn.Module):
         rnn_input = torch.cat((embedded, weighted), dim=2)
         # rnn_input = [1, batch size, enc hid dim + emb dim]
 
-        output, hidden = self.rnn(rnn_input, key.unsqueeze(0))
+        output, hidden = self.rnn(rnn_input, hidden.unsqueeze(0))
 
         # output = [sent len, batch size, dec hid dim * n directions]
         # hidden = [n layers * n directions, batch size, dec hid dim]
--- src/model/seq2seq.py
+++ src/model/seq2seq.py
@@ -5,11 +5,13 @@ import torch.nn.functional as F
 import torch
 
 class Seq2Seq(nn.Module):
-    def __init__(self, decoder, device):
+    def __init__(self, encoder, decoder, device, encoder_trained):
         super().__init__()
 
+        self.encoder = encoder
         self.decoder = decoder
         self.device = device
+        self.encoder_trained = encoder_trained
 
     def forward(self, src, trg, teacher_forcing_ratio=0.5):
         # src = [src sent len, batch size]
@@ -17,7 +19,17 @@ class Seq2Seq(nn.Module):
         # teacher_forcing_ratio is probability to use teacher forcing
         # e.g. if teacher_forcing_ratio is 0.75 we use teacher forcing 75% of the time
 
-        batch_size = src.shape[0]
+        input_ids, token_type_ids, attention_mask = src
+
+        if self.encoder_trained:
+            bert_hs = self.encoder(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+        else:
+            with torch.no_grad():
+                bert_hs = self.encoder(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+
+        bert_encodings = bert_hs[0]
+
+        batch_size = trg.shape[0]
         max_len = trg.shape[1]
         trg_vocab_size = self.decoder.output_dim
 
@@ -30,8 +42,10 @@ class Seq2Seq(nn.Module):
         # first input to the decoder is the <sos> tokens
         output = trg[:, 0]
 
+        hidden = torch.zeros(self.decoder.num_layers, output.shape[0], self.decoder.dec_hid_dim).to(self.device)
+
         for t in range(1, max_len):
-            output, hidden = self.decoder(output, src)
+            output, hidden = self.decoder(output, bert_encodings, hidden)
             outputs[:, t] = output
             teacher_force = random.random() < teacher_forcing_ratio
             # il primo 1 indica che il massimo viene cercato per ogni riga. Il secondo prende l'indice e non il valore
